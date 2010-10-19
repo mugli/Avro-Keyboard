5b640e7d61ab048050d3db1608fe876dabe789eb
Mehdi Hasan Khan <mhasan@omicronlab.com>
2010-10-19 16:55:31 +0600

    Updated skin designer project

--- Skin Designer/SkinDesigner.dpr
+++ Skin Designer/SkinDesigner.dpr
@@ -1,44 +1,41 @@
 {
   =============================================================================
   *****************************************************************************
-     The contents of this file are subject to the Mozilla Public License
-     Version 1.1 (the "License"); you may not use this file except in
-     compliance with the License. You may obtain a copy of the License at
-     http://www.mozilla.org/MPL/
+  The contents of this file are subject to the Mozilla Public License
+  Version 1.1 (the "License"); you may not use this file except in
+  compliance with the License. You may obtain a copy of the License at
+  http://www.mozilla.org/MPL/
 
-     Software distributed under the License is distributed on an "AS IS"
-     basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-     License for the specific language governing rights and limitations
-     under the License.
+  Software distributed under the License is distributed on an "AS IS"
+  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+  License for the specific language governing rights and limitations
+  under the License.
 
-     The Original Code is Avro Keyboard 5.
+  The Original Code is Avro Keyboard 5.
 
-     The Initial Developer of the Original Code is
-     Mehdi Hasan Khan <mhasan@omicronlab.com>.
+  The Initial Developer of the Original Code is
+  Mehdi Hasan Khan <mhasan@omicronlab.com>.
 
-     Copyright (C) OmicronLab <http://www.omicronlab.com>. All Rights Reserved.
+  Copyright (C) OmicronLab <http://www.omicronlab.com>. All Rights Reserved.
 
 
-     Contributor(s): ______________________________________.
+  Contributor(s): ______________________________________.
 
   *****************************************************************************
   =============================================================================
 }
-
 {$INCLUDE ../ProjectDefines.inc}
-
 program SkinDesigner;
 
 uses
   Forms,
-  ufrmSkinCreator in 'ufrmSkinCreator.pas' {frmSkinCreator},
-  ufrmAbout in 'ufrmAbout.pas' {frmAbout},
-  uFrameImageAdd in 'uFrameImageAdd.pas' {FrameImageAdd: TFrame},
-  uFrameDrag in 'uFrameDrag.pas' {FrameDrag: TFrame},
+  ufrmSkinCreator in 'ufrmSkinCreator.pas' { frmSkinCreator } ,
+  ufrmAbout in 'ufrmAbout.pas' { frmAbout } ,
+  uFrameImageAdd in 'uFrameImageAdd.pas' { FrameImageAdd: TFrame } ,
+  uFrameDrag in 'uFrameDrag.pas' { FrameDrag: TFrame } ,
   uFileFolderHandling in '..\Keyboard and Spell checker\Units\uFileFolderHandling.pas',
   clsFileVersion in '..\Keyboard and Spell checker\Classes\clsFileVersion.pas',
   nativexml in '..\Keyboard and Spell checker\Units\nativexml.pas';
-
 {$R *.res}
 
 begin
@@ -47,4 +44,5 @@ begin
   Application.Title := 'Skin Designer';
   Application.CreateForm(TfrmSkinCreator, frmSkinCreator);
   Application.Run;
+
 end.
