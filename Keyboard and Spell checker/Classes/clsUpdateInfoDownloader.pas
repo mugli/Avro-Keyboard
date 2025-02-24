d600e9a053ef63874ca8314248777eb403880b05
jayed@codepotro <42943920+JayedAhsan@users.noreply.github.com>
2025-02-24 21:28:16 +0600

    Migrate to TNetHTTPClient
    Replace ICS with Delphi HTTP Client

--- Keyboard and Spell checker/Classes/clsUpdateInfoDownloader.pas
+++ Keyboard and Spell checker/Classes/clsUpdateInfoDownloader.pas
@@ -5,272 +5,241 @@
      Version 1.1 (the "License"); you may not use this file except in
      compliance with the License. You may obtain a copy of the License at
      http://www.mozilla.org/MPL/
-
      Software distributed under the License is distributed on an "AS IS"
      basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
      License for the specific language governing rights and limitations
      under the License.
-
      The Original Code is Avro Keyboard 5.
-
      The Initial Developer of the Original Code is
      Mehdi Hasan Khan (mhasan@omicronlab.com).
-
      Copyright (C) OmicronLab (http://www.omicronlab.com). All Rights Reserved.
-
-
      Contributor(s): ______________________________________.
-
   *****************************************************************************
   =============================================================================
 }
-
 {$INCLUDE ../ProjectDefines.inc}
+unit clsUpdateInfoDownloader;
 
-Unit clsUpdateInfoDownloader;
+interface
 
-Interface
-Uses NativeXml,
-     SysUtils,
-     Classes,
-     Windows,
-     DateUtils,
-     Forms,
-     OverbyteIcsWndControl,
-     OverbyteIcsHttpProt,
-     Wininet;
+uses
+  System.SysUtils, System.Classes, System.Net.HttpClient, System.Net.URLClient,
+  System.Net.HttpClientComponent, Xml.XMLIntf, Xml.XMLDoc, System.Variants,
+  Vcl.Forms, Windows,  Winapi.WinInet;
 
+type
+  TUpdateCheck = class
+  private
+    HttpClient: TNetHTTPClient;
+    HttpRequest: TNetHTTPRequest;
+    XML: IXMLDocument;
+    StillDownloading: Boolean;
+    Verbose: Boolean;
 
-Type
-     TUpdateCheck = Class
-     Private
-          Downloader: THttpCli;
-          MemS: TMemoryStream;
-          XML: TNativeXML;
-          StillDownloading: Boolean;
-          Verbose: Boolean;
+    function IsUpdate(const Major, Minor, Release, Build: Integer): Boolean;
+    procedure ProcessResponse(const Response: string);
+  public
+    function IsConnected: Boolean;
+    procedure Check;
+    procedure CheckSilent;
+    constructor Create;
+    destructor Destroy; override;
+  end;
 
-          Function IsUpdate(Const Major, Minor, Release, Build: Integer): Boolean;
-          Procedure FinishDownload(Sender: TObject; RqType: THttpRequest; Error: Word);
-     Public
+var
+  Updater: TUpdateCheck;
 
+const
+  {$IFDEF BetaVersion}
+    {$IFDEF PortableOn}
+      UpdateInfo = 'http://www.omicronlab.com/download/liveupdate/portable_avrokeyboard/versioninfo_beta.xml';
+    {$ELSE}
+      UpdateInfo = 'http://www.omicronlab.com/download/liveupdate/avrokeyboard/versioninfo_beta.xml';
+    {$ENDIF}
+  {$ELSE}
+    {$IFDEF PortableOn}
+      UpdateInfo = 'http://www.omicronlab.com/download/liveupdate/portable_avrokeyboard/versioninfo.xml';
+    {$ELSE}
+      UpdateInfo = 'http://www.omicronlab.com/download/liveupdate/avrokeyboard/versioninfo.xml';
+    {$ENDIF}
+  {$ENDIF}
 
-          Function IsConnected: Boolean;
-          Procedure Check;
-          Procedure CheckSilent;
-          Constructor Create;
-          Destructor Destroy; Override;
-     End;
+implementation
 
-Var
-     Updater                  : TUpdateCheck;
-
-Const
-     {$IFDEF BetaVersion}
-
-     {$IFDEF PortableOn}
-     UpdateInfo               = 'http://www.omicronlab.com/download/liveupdate/portable_avrokeyboard/versioninfo_beta.xml';
-     {$ELSE}
-     UpdateInfo               = 'http://www.omicronlab.com/download/liveupdate/avrokeyboard/versioninfo_beta.xml';
-     {$ENDIF}
-
-     {$ELSE}
-
-     {$IFDEF PortableOn}
-     UpdateInfo               = 'http://www.omicronlab.com/download/liveupdate/portable_avrokeyboard/versioninfo.xml';
-     {$ELSE}
-     UpdateInfo               = 'http://www.omicronlab.com/download/liveupdate/avrokeyboard/versioninfo.xml';
-     {$ENDIF}
-
-     {$ENDIF}
-
-Implementation
-
-Uses
-     clsFileVersion,
-     ufrmUpdateNotify,
-     uWindowHandlers;
-
-
+uses
+  clsFileVersion, ufrmUpdateNotify, uWindowHandlers, System.Threading;
 
 { TUpdateCheck }
 {===============================================================================}
-
 Procedure TUpdateCheck.check;
-Begin
-     If StillDownloading Then
-          exit
-     Else Begin
-          MemS := TMemoryStream.Create;
+begin
+  if StillDownloading then Exit;
 
-          Verbose := True;
-          Downloader.URL := UpdateInfo;
-          Downloader.RcvdStream := MemS;
-          Downloader.GetASync;
-          StillDownloading := True;
-     End;
-End;
+  Verbose := True;
+  StillDownloading := True;
+
+  TTask.Run(
+    procedure
+    var
+      Response: string;
+    begin
+      try
+        Response := HttpClient.Get(UpdateInfo).ContentAsString();
+        TThread.Queue(nil,
+          procedure
+          begin
+            ProcessResponse(Response);
+          end
+        );
+      except
+        on E: Exception do
+        begin
+          StillDownloading := False;
+          if Verbose then
+            Application.MessageBox(
+              'There was an error checking for updates. Please try again later.',
+              'Error', MB_OK or MB_ICONHAND or MB_DEFBUTTON1 or MB_SYSTEMMODAL);
+        end;
+      end;
+    end
+  );
+end;
 
 {===============================================================================}
 
 Procedure TUpdateCheck.CheckSilent;
 Begin
-     If StillDownloading Then
-          exit
-     Else Begin
-          MemS := TMemoryStream.Create;
+  if StillDownloading then Exit;
 
-          Verbose := False;
-          Downloader.URL := UpdateInfo;
-          Downloader.RcvdStream := MemS;
-          Downloader.GetASync;
-          StillDownloading := True;
-     End;
+  Verbose := False;
+  StillDownloading := True;
+
+  TTask.Run(
+    procedure
+    var
+      Response: string;
+    begin
+      try
+        Response := HttpClient.Get(UpdateInfo).ContentAsString();
+        TThread.Queue(nil,
+          procedure
+          begin
+            ProcessResponse(Response);
+          end
+        );
+      except
+        on E: Exception do
+          StillDownloading := False;
+      end;
+    end
+  );
 End;
+
+
+constructor TUpdateCheck.Create;
+begin
+  inherited;
+  HttpClient := TNetHTTPClient.Create(nil);
+  HttpRequest := TNetHTTPRequest.Create(nil);
+  HttpRequest.Client := HttpClient;
+  HttpClient.UserAgent := 'Avro Keyboard';
+  HttpClient.AllowCookies := True;
+  StillDownloading := False;
+end;
 
 {===============================================================================}
 
-Constructor TUpdateCheck.Create;
-Begin
-     // Execute the parent (TObject) constructor first
-     Inherited;                         // Call the parent Create method
-     Downloader := THttpCli.Create(Nil);
-     Downloader.RequestVer := '1.0';
-     Downloader.Agent := 'User-Agent: Avro Keyboard';
-     Downloader.NoCache := True;
-     Downloader.OnRequestDone := FinishDownload;
+destructor TUpdateCheck.Destroy;
+begin
+  HttpRequest.Free;
+  HttpClient.Free;
+  inherited;
+end;
 
-     StillDownloading := False;
-End;
 
 {===============================================================================}
 
-Destructor TUpdateCheck.Destroy;
-Begin
-     FreeAndNil(Downloader);
-     Inherited;
-End;
+procedure TUpdateCheck.ProcessResponse(const Response: string);
+var
+  Major, Minor, Release, Build: Integer;
+  changelogurl, downloadurl, productpageurl, releasedate: string;
+begin
+  StillDownloading := False;
+
+  try
+    XML := TXMLDocument.Create(nil);
+    XML.LoadFromXML(Response);
+    XML.Active := True;
+
+    // Extracting update information
+    Major := XML.DocumentElement.ChildNodes['versionmajor'].NodeValue;
+    Minor := XML.DocumentElement.ChildNodes['versionminor'].NodeValue;
+    Release := XML.DocumentElement.ChildNodes['versionrevision'].NodeValue;
+    if Assigned(XML.DocumentElement.ChildNodes.FindNode('versionbuild')) then
+      Build := XML.DocumentElement.ChildNodes['versionbuild'].NodeValue
+    else
+      Build := 0;  // Default for Avro 4.x compatibility
+
+    changelogurl := XML.DocumentElement.ChildNodes['changelogurl'].NodeValue;
+    downloadurl := XML.DocumentElement.ChildNodes['downloadurl'].NodeValue;
+    productpageurl := XML.DocumentElement.ChildNodes['productpageurl'].NodeValue;
+    releasedate := XML.DocumentElement.ChildNodes['releasedate'].NodeValue;
+
+    if IsUpdate(Major, Minor, Release, Build) then
+    begin
+      CheckCreateForm(TfrmUpdateNotify, frmUpdateNotify, 'frmUpdateNotify');
+      frmUpdateNotify.SetupAndShow(
+        Format('%d.%d.%d.%d', [Major, Minor, Release, Build]),
+        releasedate, changelogurl, downloadurl);
+    end
+    else if Verbose then
+    begin
+      Application.MessageBox(
+        'You are using the latest version of Avro Keyboard.' + #10 +
+        'No update is available at this moment.',
+        'Avro Keyboard', MB_OK or MB_ICONEXCLAMATION or MB_DEFBUTTON1 or MB_SYSTEMMODAL);
+    end;
+
+  except
+    on E: Exception do
+    begin
+      if Verbose then
+        Application.MessageBox(
+          'There was an error processing update information. Please try again later.',
+          'Error', MB_OK or MB_ICONHAND or MB_DEFBUTTON1 or MB_SYSTEMMODAL);
+    end;
+  end;
+end;
 
 {===============================================================================}
-     {$HINTS Off}
-Procedure TUpdateCheck.FinishDownload(Sender: TObject; RqType: THttpRequest; Error: Word);
+
+function TUpdateCheck.IsConnected: Boolean;
 Var
-     Major, Minor, Release, Build: Integer;
-     changelogurl, downloadurl, productpageurl, releasedate: String;
+  dwConnectionTypes: DWORD;
 Begin
+  Result := False;
+  dwConnectionTypes := INTERNET_CONNECTION_MODEM + INTERNET_CONNECTION_LAN +
+    INTERNET_CONNECTION_PROXY;
 
-     Major := 0;
-     Minor := 0;
-     Release := 0;
-     Build := 0;
-
-     StillDownloading := False;
-
-     If (Downloader.StatusCode = 200) And (Error = 0) Then Begin
-          XML := TNativeXML.Create;
-          XML.ExternalEncoding := seUTF8;
-          XML.LoadFromStream(MemS);
-
-          Try
-               Try
-                    Major := XML.Root.FindNode('versionmajor').ValueAsInteger;
-                    Minor := XML.Root.FindNode('versionminor').ValueAsInteger;
-                    Release := XML.Root.FindNode('versionrevision').ValueAsInteger;
-                    If XML.Root.FindNode('versionbuild') <> Nil Then //Avro 4.x compatibility
-                         Build := XML.Root.FindNode('versionbuild').ValueAsInteger;
-
-                    changelogurl := XML.Root.FindNode('changelogurl').ValueAsUnicodeString;
-                    downloadurl := XML.Root.FindNode('downloadurl').ValueAsUnicodeString;
-                    productpageurl := XML.Root.FindNode('productpageurl').ValueAsUnicodeString;
-                    releasedate := XML.Root.FindNode('releasedate').ValueAsUnicodeString;
-               Except
-                    On e: exception Do Begin
-                         If Verbose Then
-                              Application.MessageBox('There was an error checking update for Avro Keyboard.'
-                                   + #10 + 'Please try again later.', 'Ooops!',
-                                   MB_OK + MB_ICONHAND + MB_DEFBUTTON1 + MB_SYSTEMMODAL);
-                         Exit;
-                    End;
-               End;
-          Finally
-               FreeAndNil(XML);
-               FreeAndNil(MemS);
-          End;
-
-          If IsUpdate(Major, Minor, Release, Build) = True Then Begin
-               CheckCreateForm(TfrmUpdateNotify, frmUpdateNotify, 'frmUpdateNotify');
-               frmUpdateNotify.SetupAndShow(IntToStr(Major) + '.' + IntToStr(Minor) + '.' +
-                    IntToStr(Release) + '.' + IntToStr(Build), releasedate, changelogurl, downloadurl);
-          End
-          Else Begin
-               If Verbose Then
-                    Application.MessageBox('You are using the latest version of Avro Keyboard.'
-                         + #10 + 'No update is available at this moment', 'Avro Keyboard', MB_OK + MB_ICONEXCLAMATION + MB_DEFBUTTON1 + MB_SYSTEMMODAL);
-          End;
-     End
-     Else Begin
-          If Verbose Then
-               Application.MessageBox('There was an error checking update for Avro Keyboard.'
-                    + #10 + 'Please try again later.', 'Ooops!',
-                    MB_OK + MB_ICONHAND + MB_DEFBUTTON1 + MB_SYSTEMMODAL);
-     End;
-
-
-
-
-End;
+  Result := InternetGetConnectedState(@dwConnectionTypes, 0);
+end;
 
 {===============================================================================}
 
-Function TUpdateCheck.IsConnected: Boolean;
-Var
-     dwConnectionTypes        : DWORD;
-Begin
-     Result := False;
-     dwConnectionTypes :=
-          INTERNET_CONNECTION_MODEM +
-          INTERNET_CONNECTION_LAN +
-          INTERNET_CONNECTION_PROXY;
+function TUpdateCheck.IsUpdate(const Major, Minor, Release, Build: Integer): Boolean;
+var
+  Version: TFileVersion;
+begin
+  Version := TFileVersion.Create;
+  try
+    Result := (Major > Version.VerMajor) or
+              ((Major = Version.VerMajor) and (Minor > Version.VerMinor)) or
+              ((Major = Version.VerMajor) and (Minor = Version.VerMinor) and (Release > Version.VerRelease)) or
+              ((Major = Version.VerMajor) and (Minor = Version.VerMinor) and (Release = Version.VerRelease) and (Build > Version.VerBuild));
+  finally
+    Version.Free;
+  end;
+end;
 
-     Result := InternetGetConnectedState(@dwConnectionTypes, 0);
-End;
-
-{===============================================================================}
-
-Function TUpdateCheck.IsUpdate(Const Major, Minor, Release,
-     Build: Integer): Boolean;
-Var
-     Version                  : TFileVersion;
-Begin
-     Version := TFileVersion.Create();
-     Result := False;
-
-     Try
-          If Major > Version.VerMajor Then Begin
-               Result := True;
-          End
-          Else If Major = Version.VerMajor Then Begin
-               If Minor > Version.VerMinor Then Begin
-                    Result := True;
-               End
-               Else If Minor = Version.VerMinor Then Begin
-                    If Release > Version.VerRelease Then Begin
-                         Result := True;
-                    End
-                    Else If Release = Version.VerRelease Then Begin
-                         If Build > Version.VerBuild Then Begin
-                              Result := True;
-                         End;
-                    End;
-               End;
-          End;
-
-     Finally
-          Version.Free;
-     End;
-
-End;
- {$HINTS On}
- 
-End.
+end.
 
