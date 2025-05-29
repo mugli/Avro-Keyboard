478f01600f64c530e4ed124d5ea45b3e80cbc0c5
Mehdi Hasan Khan <mhasan@omicronlab.com>
2025-05-29 17:28:29 +0200

    move to MPL 2.0 license

--- Keyboard and Spell checker/Classes/clsUpdateInfoDownloader.pas
+++ Keyboard and Spell checker/Classes/clsUpdateInfoDownloader.pas
@@ -1,22 +1,11 @@
 {
   =============================================================================
-  *****************************************************************************
-  The contents of this file are subject to the Mozilla Public License
-  Version 1.1 (the "License"); you may not use this file except in
-  compliance with the License. You may obtain a copy of the License at
-  https://www.mozilla.org/MPL/
-  Software distributed under the License is distributed on an "AS IS"
-  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-  License for the specific language governing rights and limitations
-  under the License.
-  The Original Code is Avro Keyboard 5.
-  The Initial Developer of the Original Code is
-  Mehdi Hasan Khan (mhasan@omicronlab.com).
-  Copyright (C) OmicronLab (https://www.omicronlab.com). All Rights Reserved.
-  Contributor(s): ______________________________________.
-  *****************************************************************************
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at https://mozilla.org/MPL/2.0/.
   =============================================================================
 }
+
 {$INCLUDE ../ProjectDefines.inc}
 unit clsUpdateInfoDownloader;
 
